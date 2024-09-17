b84b63f0391c6dbc6e6c614cc2bc1b714691ef94
Tushar nama <2021kucp1111@iiitkota.ac.in>
2024-09-18 03:54:17 +0530

    my first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "TUSHAR NAMA",
+  profile: "/profile.jpeg",
+  designation: "Software Developer",
+  description:
+    "My name is TUSHAR NAMA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "tusharnama844@gmail.com",
+  phone: "+91 6378518145",
+  address: "398, Sanganer, Jaipur, Rajasthan, 302029 ",
+  github: "https://github.com/tusharnama24",
+  facebook: "https://www.facebook.com/",
+  linkedIn: "https://www.linkedin.com/feed/",
+  twitter: "https://twitter.com/",
+  stackOverflow: "https://stackoverflow.com/",
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "tusharnama24",
+  resume:
+    "https://drive.google.com/file/d/1yGZPF7yNfFpsNVFjo9BtiR7Cx7GgJ3nd/view?usp=drive_link",
+};
---
+++ utils/data/projects-data.js
@@ -0,0 +1,62 @@
+import ayla from '/public/image/ayla.jpg';
+import crefin from '/public/image/crefin.jpg';
+import realEstate from '/public/image/real-estate.jpg';
+import travel from '/public/image/travel.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        name: 'ThePahnawa',
+        description: "As part of my startup, I designed and developed a full-stack e-commerce website dedicated to selling clothing. The platform is built using modern web technologies such as React for a dynamic front-end experience and MongoDB for a scalable backend database. The website is fully responsive and optimized for user engagement, focusing on delivering a seamless shopping experience. With an intuitive user interface and smooth navigation, this project showcases my skills in building full-stack applications, especially in the e-commerce domain.",
+        tools: ['HTML', 'CSS', 'JAVASCRIPT', 'REACT',  'MongoDB'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        image: crefin,
+    },
+    {
+        id: 2,
+        name: 'Voilence Detection',
+        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
+        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        image: travel,
+    },
+    {
+        id: 3,
+        name: 'AI Voice Assistant',
+        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        code: '',
+        role: 'Full Stack Developer',
+        demo: '',
+        image: realEstate,
+    },
+    {
+        id: 4,
+        name: 'Newsroom Management',
+        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+        code: '',
+        demo: '',
+        image: ayla,
+        role: 'Full Stack Developer',
+    }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+//     image: crefin,
+// },
